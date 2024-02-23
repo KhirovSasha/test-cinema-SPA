483fbf7596f5dac4c1712ef71e6f85c3908d17c6
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-23 14:44:23 +0200

    created factories for models

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->title(),
+            'url' => Storage::url('public/default-image.jpg'),
+            'publish_status' => fake()->randomElement(['published', 'underConsideration'])
+        ];
+    }
+}

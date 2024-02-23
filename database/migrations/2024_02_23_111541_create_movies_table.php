709954c60b55829982bd4f1b3e28f6c330814661
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-23 13:42:41 +0200

    created tables movies. genres, genre to movie

--- database/migrations/2024_02_23_111541_create_movies_table.php
+++ database/migrations/2024_02_23_111541_create_movies_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('movies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('url');
+            $table->enum('publish_status', ['published', 'underConsideration'])->default('underConsideration');
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('films');
     }
 };

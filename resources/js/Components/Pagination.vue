627b59c2afca575f514c1669666ddcb8be8f4e79
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-25 19:49:03 +0200

    added component for pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="links.length > 3" class="flex justify-center mt-4 space-x-4">
+        <div class="flex flex-wrap -mb-1">
+            <template v-for="link in links" :key="link.label">
+                <div
+                    v-if="link.url === null"
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
+                    v-html="link.label"
+                ></div>
+                <a
+                    v-else-if="link.url"
+                    @click.prevent="fetchData(link.url)"
+                    class="px-4 py-3 text-sm leading-4 bg-white rounded hover:bg-white focus:text-indigo-500 hover:shadow"
+                    :class="{ 'bg-indigo-400 text-red': link.active }"
+                    v-html="link.label"
+                ></a>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+defineProps({
+    links: Array,
+    fetchData: Function,
+});
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,16 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import AppComponent from './App.vue';
+import Pagination from './Components/Pagination.vue'
 import router from './router/index.js';
 
 const app = createApp({
     components: {
-        AppComponent
+        AppComponent,
     }
 });
+
+app.component('pagination', Pagination)
 
 
 app.use(router);

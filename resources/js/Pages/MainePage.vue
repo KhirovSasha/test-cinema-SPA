b2f727e485d4f20d40717fc63f31c1d2d6d8981d
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-26 03:36:01 +0200

    added links on edit, info and deleted button

--- resources/js/Pages/MainePage.vue
+++ resources/js/Pages/MainePage.vue
@@ -16,6 +16,14 @@
                 Add Genre
             </button>
         </router-link>
+        <router-link to="/genres">
+            <button
+                type="button"
+                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+                Genres
+            </button>
+        </router-link>
         <div v-if="paginationMovies && paginationMovies.meta">
             <pagination
                 :links="paginationMovies.meta.links"
@@ -79,13 +87,13 @@
                         </td>
 
                         <td class="py-2 px-4">
-                            <router-link to="/createMovie" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Info</router-link>
+                            <router-link :to='"/infoMovie/" + movie.id' class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Info</router-link>
                         </td>
                         <td class="py-2 px-4 ">
-                            <router-link to="/createMovie" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Edit</router-link>
+                            <router-link :to='"/editMovie/" + movie.id' class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Edit</router-link>
                         </td>
                         <td class="py-2 px-4">
-                            <router-link to="/createMovie" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</router-link>
+                            <span @click="deleteMovie(movie.id)" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</span>
                         </td>
                     </tr>
                 </table>
@@ -111,6 +119,16 @@ export default {
             }
         };
 
+        const deleteMovie = (id) => {
+            axios.delete(`/api/movie/${id}/delete`)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                    console.log(err);
+                });
+        };
+
         onMounted(() => {
             axios
                 .get("api/pagination")
@@ -123,7 +141,7 @@ export default {
                 });
         });
 
-        return { paginationMovies, fetchPaginationData };
+        return { paginationMovies, fetchPaginationData, deleteMovie };
     },
 };
 </script>
